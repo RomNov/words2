a7a3b66630c84c1bcf49cee447896c66b4551692
M0ogen <romkanov@gmail.com>
2018-06-17 14:54:33 +0300

    fix bug3

--- app/src/main/java/ru/moogen/words/DataHelper.java
+++ app/src/main/java/ru/moogen/words/DataHelper.java
@@ -15,7 +15,7 @@ import java.util.GregorianCalendar;
 public class DataHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "words.db";
-    public static final int DATABASE_VERSION = 26;
+    public static final int DATABASE_VERSION = 27;
 
     public static final String TABLE_WORDS_NAME = "words";
 
